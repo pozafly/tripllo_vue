79b529d56061efcb96a6570d83f73c410a987ff6
HwangSunTae <pozafly@gmail.com>
2020-12-08 01:26:11 +0900

    login 페이지 스타일링

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -4,22 +4,45 @@
       <i class="fas fa-suitcase"></i>
       <span class="title">Tripllo</span>
     </header>
-    <main>
-      <span>Log in to Tripllo</span>
-      <form class="form">
-        <div class="input">
-          <input type="text" />
-          <input type="text" />
-          <button type="submit">Log in</button>
+    <div class="main_container">
+      <main>
+        <div class="container">
+          <span class="login_text"><b>Log in to Tripllo</b></span>
+          <form>
+            <div class="submit_items">
+              <input
+                class="submit_item"
+                type="text"
+                placeholder="Enter email"
+              />
+              <input
+                class="submit_item"
+                type="text"
+                placeholder="Enter password"
+              />
+              <button class="submit_item btn" type="submit">
+                <b>Log in</b>
+              </button>
+            </div>
+            <div class="text">OR</div>
+          </form>
+          <div class="external_login_container">
+            <button class="external_item">
+              <img src="@/assets/logo/google.png" />
+              Continue with Google
+            </button>
+            <button class="external_item">
+              <img src="@/assets/logo/facebook.png" />
+              Continue with FaceBook
+            </button>
+            <button class="external_item">
+              <img src="@/assets/logo/naver.png" />
+              Continue with Naver
+            </button>
+          </div>
         </div>
-        <span>OR</span>
-      </form>
-      <div class="external-login">
-        <button>Continue with Google</button>
-        <button>Continue with FaceBook</button>
-        <button>Continue with Naver</button>
-      </div>
-    </main>
+      </main>
+    </div>
     <aside>
       <a href="">Privacy Policy</a>
     </aside>
@@ -42,10 +65,22 @@ export default {};
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap');
 * {
-  border: 1px solid black;
+  /* border: 1px solid black; */
+}
+button {
+  border: 0;
+  border-radius: 3px;
+  box-shadow: rgba(0, 0, 0, 0.2) 1px 1px 5px 0;
 }
 .page {
   background: #f9fafc;
+}
+.btn {
+  background: #5aac44;
+  color: white;
+}
+.btn:hover {
+  background: #60bd4e;
 }
 header {
   font-family: 'Pacifico', cursive;
@@ -59,17 +94,56 @@ header {
 header .fas.fa-suitcase {
   font-size: 4rem;
 }
+.main_container {
+  display: flex;
+  justify-content: center;
+}
 main {
+  border: 1px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 30rem;
+}
+main .container {
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  width: 23rem;
+  padding: 2rem;
+}
+main .container .login_text {
+  text-align: center;
+  font-size: 1.2rem;
+}
+main .container .submit_items {
+  display: flex;
+  flex-direction: column;
+  height: 14rem;
+  justify-content: space-around;
+}
+main .container .submit_items .submit_item {
+  height: 2.3rem;
+}
+main .container .text {
+  text-align: center;
+  font-size: 0.8rem;
+}
+main .container .external_login_container {
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  align-items: center;
+  height: 14rem;
 }
-.input > input,
-.input > button {
+img {
+  width: 1.3rem;
+  padding-right: 0.3rem;
+}
+main .container .external_login_container .external_item {
+  height: 2.3rem;
+  background-color: white;
   display: flex;
-  margin: 2rem;
-  flex-direction: column;
-  align-items: space-between;
+  justify-content: center;
+  align-items: center;
 }
 </style>
