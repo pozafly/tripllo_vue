0724e80fffa51a6f8b761f0361539736f65f0b14
HwangSunTae <pozafly@gmail.com>
2021-02-13 14:20:21 +0900

    파일 다운로드 권한 부여

--- src/components/card/cardDetail/detailItems/detailAttachment/AttachmentList.vue
+++ src/components/card/cardDetail/detailItems/detailAttachment/AttachmentList.vue
@@ -15,13 +15,20 @@
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
 
 export default {
   props: ['item'],
+  computed: {
+    ...mapState(['board', 'user']),
+  },
   methods: {
     ...mapActions(['DELETE_FILE']),
     download(e) {
+      if (!this.permission()) {
+        alert('권한이 없습니다.');
+        return;
+      }
       if (e.target.className === 'file-delete-btn') return;
       let download = window.confirm(
         `${this.item.fileName}을 다운로드 받으시겠습니까?`,
@@ -29,10 +36,21 @@ export default {
       if (download) location.href = `${this.item.link}`;
     },
     deleteFile() {
+      if (!this.permission()) {
+        alert('권한이 없습니다.');
+        return;
+      }
       let deleteFile = window.confirm(
         `${this.item.fileName} 파일을 삭제하시겠습니까?`,
       );
       if (deleteFile) this.DELETE_FILE(this.item.id);
+    },
+    permission() {
+      console.log(this.board.invitedUser);
+      if (this.board.createdBy === this.user.id) return true;
+      if (this.board.invitedUser) {
+        if (this.board.invitedUser.includes(this.user.id)) return true;
+      } else return false;
     },
   },
 };
