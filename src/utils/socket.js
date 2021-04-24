669bdd4d83b160ba45f47e449d57b9e04e47abb2
HwangSunTae <pozafly@gmail.com>
2021-04-24 13:52:21 +0900

    URL LOCAL 가능

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -6,7 +6,8 @@ let socketInstance = null;
 
 const socketConnect = () => {
   const baseURL =
-    process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_URL : '';
+    // process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_URL : '';
+    process.env.VUE_APP_API_URL;
 
   try {
     if (socketInstance === null) {
