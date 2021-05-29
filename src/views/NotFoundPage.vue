59aeebc9d4daa271f7df73db2b79e2a9fcefc2d1
HwangSunTae <pozafly@gmail.com>
2021-05-29 20:46:47 +0900

    [REFACTOR] Semantic-Tag NotFoundPage apply

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="container">
     <CommonHeader />
-    <div class="notfound-wrap">
-      <awesome icon="skull" class="notfound-icon" />
-      <span class="notfound-text">해당 페이지를 찾을 수 없습니다.</span>
-      <a href="" @click.prevent="goBack">메인페이지로 이동</a>
-    </div>
+    <main class="main-wrap">
+      <section class="notfound-wrap">
+        <awesome icon="skull" class="notfound-icon" />
+        <h2 class="notfound-text">해당 페이지를 찾을 수 없습니다.</h2>
+        <a href="" @click.prevent="goBack">메인페이지로 이동</a>
+      </section>
+    </main>
   </div>
 </template>
 
@@ -29,26 +31,28 @@ export default {
 .container {
   height: 100%;
   min-height: 100%;
-  margin-top: -40px;
   padding-top: 40px;
   box-sizing: border-box;
-  .notfound-wrap {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    margin: 0 auto;
-    width: 100%;
+  .main-wrap {
     height: 100%;
-    .notfound-icon {
-      display: block;
-      width: 400px;
-      font-size: 155px;
-      margin-bottom: 70px;
-    }
-    .notfound-text {
-      font-size: 20px;
-      margin-bottom: 15px;
+    .notfound-wrap {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      margin: 0 auto;
+      width: 100%;
+      height: 100%;
+      .notfound-icon {
+        display: block;
+        width: 400px;
+        font-size: 155px;
+        margin-bottom: 70px;
+      }
+      .notfound-text {
+        font-size: 20px;
+        margin-bottom: 15px;
+      }
     }
   }
 }
